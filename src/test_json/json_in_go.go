bc24773e96c3866825c9bfba8c04ee0983c3d0ad
yangxing <yangxing@gosun.com>
2018-11-30 02:06:03 -0800

    add test_json

--- src/test_json/json_in_go.go
+++ src/test_json/json_in_go.go
@@ -25,18 +25,18 @@ type List []interface{}
 
 func checkErr(err error) {
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ERROR:", err)
 	}
 }
 
-func receive_json(resp http.ResponseWriter, req *http.Request) {
+func receive_json_1(resp http.ResponseWriter, req *http.Request) {
 	var req_json map[string]interface{}
 
-	// 1.parse args in req json:
+	// 1.use type assertion parse args in req json:
+	fmt.Println("[use type assertion]:")
 	decoder := json.NewDecoder(req.Body)
 	err := decoder.Decode(&req_json)
 	checkErr(err)
-	fmt.Println("[use type assertion]:")
 	fmt.Println("--> receive request json:", req_json)
 	fmt.Println("--> get name:", req_json["name"].(string))
 	fmt.Println("--> get money:", req_json["money"].(float64))
@@ -44,11 +44,15 @@ func receive_json(resp http.ResponseWriter, req *http.Request) {
 	fmt.Println("--> get list[0]:", req_json["list"].([]interface{})[0].(float64))
 	fmt.Println("--> get map:", req_json["map"].(map[string]interface{}))
 	fmt.Println("--> get map[k]:", req_json["map"].(map[string]interface{})["k"].(string))
+}
 
+func receive_json_2(resp http.ResponseWriter, req *http.Request) {
 	// 2.use simplejson to parse req json:
+	//   see https://godoc.org/github.com/bitly/go-simplejson
+	// actually, simplejson use type assertion inside too...
+	fmt.Println("\n\n[use simplejson]:")
 	js, err := simplejson.NewFromReader(req.Body)
 	checkErr(err)
-	fmt.Println("\n\n [use simplejson]:")
 	fmt.Println("--> receive request json:", js)
 	name, err := js.Get("name").String()
 	checkErr(err)
@@ -72,7 +76,8 @@ func send_json(resp http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/r", receive_json)
+	http.HandleFunc("/r1", receive_json_1)
+	http.HandleFunc("/r2", receive_json_2)
 	http.HandleFunc("/s", send_json)
 	fmt.Println("Server Started at :8000")
 	http.ListenAndServe(":8000", nil)
