93952fd89bf87fa120a3a6125cac962da4ef2703
yxzoro <yangxing20107@gmail.com>
2019-07-11 11:26:17 +0800

    .

--- atomic/main.go
+++ atomic/main.go
@@ -11,17 +11,22 @@ func main() {
 	log.Println("test atomic, i = 0")
 	var i int64 = 0
 	for x := 0; x < 100; x++ {
-		go change(&i, x)
+		go write(&i, x)
+		go read(&i, x)
 	}
 
 	time.Sleep(2*time.Second)
 	log.Println("end, i = ", i)
 }
 
-func change(i *int64, x int) {
+func write(i *int64, x int) {
 	log.Println(x, " add 1 to i")
-	atomic.AddInt64(i, 1)
+	atomic.AddInt64(i, 1)  // atomic write 
 	log.Println(x, " add 1 to i ok")
+}
+
+func read(i *int64, x int) {
+	log.Println(x, " read i = ", atomic.LoadInt64(i))  // atomic read
 }
 
 // atomic make sure that: i = 100
