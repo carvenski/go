6dbdbc69ce93245d5fc7e6e01e92544a048c0c2d
Michael Yang <yangxing20107@gmail.com>
2020-06-09 10:32:46 +0800

    Update decorator_example.go

--- decorator/decorator_example.go
+++ decorator/decorator_example.go
@@ -7,6 +7,11 @@ import (
 	"time"
 )
 
+// golang中的装饰器是不能装饰任意函数的,
+// 一个装饰器必须针对一类有固定参数和返回值的函数来写,因为类型不是动态的,
+// 那么做法就是在程序的最外层给每个请求定义一个Controller函数,它们的参数和返回都相同,
+// 然后转装饰器就可以加在它们上面了.
+
 // HTTP Server SafeHandler decorator
 func safeHandler(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
