23d710864bfb6ad4694516718ed0636636dcba54
yangxing <yangxing@mmtrix.com>
2021-03-11 19:22:57 +0800

    .

--- web_framework/echo_test/go.mod
+++ web_framework/echo_test/go.mod
@@ -3,6 +3,7 @@ module myapp
 go 1.13
 
 require (
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.2.1
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
--- web_framework/echo_test/main.go
+++ web_framework/echo_test/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"myapp/app"
 
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4" //使用echo v4版本
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	// CORS middleware
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+	}))
+	// init url router
 	app.InitRouter(e)
+	// server start
 	e.Logger.Fatal(e.Start(":30000"))
 }
